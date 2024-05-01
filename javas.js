f799c4d2660b3114d07d403e46c710b707b2a441
AmirSharofov <mister.amir.777@gmail.com>
2024-05-01 18:31:09 +0500

    create js

---
+++ javas.js
@@ -0,0 +1,20 @@
+const a = [1, 2, 2, 4]
+// a.push = ('hello world')- добавление в конец масива
+
+// pop - удаление с конца массива 
+a.pop()
+
+//shift - удаление с нацала массива 
+a.shift()
+
+// unshift-добавление в начало масива 
+a.unshift('hello world')
+
+console.log(a.indexOf(true));
+console.log(a.at(4));
+
+// splice - вырезать массив 
+console.log(a.slice(0, 4, ));
+console.log(a.slice(0, 4, 'hello')); //replace-меняет
+
+             
